4e7b9616d971a0b491e0fbfce1f5186c079bcb11
Aliaksei Fezhanka <fezhenko@gmail.com>
2021-11-10 23:32:57 +0300

    update a few files.  Pickle a file and add to db.

---
+++ Interact with SQL/postgre_sql_basic.py
@@ -0,0 +1,51 @@
+import pyscopg2
+
+
+# 1st step connect to DB
+# 2nd step create a cursor object
+# 3rd step Write an SQL query
+# 4rd step Commit changes
+# 5th step Close database connection
+
+def create_table():
+    conn = sqlite3.connect('lite.db')
+    curs = conn.cursor()
+    curs.execute("""CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)""")
+    conn.commit()
+    conn.close()
+
+
+def insert_to_db(item, quantity, price):
+    conn = sqlite3.connect('lite.db')
+    curs = conn.cursor()
+    curs.execute("""INSERT INTO store VALUES(?,?,?)""", (item, quantity, price))
+    conn.commit()
+    conn.close()
+
+
+def view():
+    conn = sqlite3.connect('lite.db')
+    curs = conn.cursor()
+    curs.execute("""SELECT * FROM store""")
+    rows = curs.fetchall()
+    conn.close()
+    return rows
+
+
+def delete_from_db(item):
+    conn = sqlite3.connect('lite.db')
+    curs = conn.cursor()
+    curs.execute("""DELETE FROM store WHERE item=?""", (item,))
+    conn.commit()
+    conn.close()
+
+
+def update_db(quantity, price, item):
+    conn = sqlite3.connect('lite.db')
+    curs = conn.cursor()
+    curs.execute("""UPDATE store SET quantity=?, price=? WHERE item=?""", (quantity, price, item))
+    conn.commit()
+    conn.close()
+
+
+print(view())
---
+++ tkinter/tkinter_basic.py
@@ -0,0 +1,42 @@
+from tkinter import *
+
+# create a window
+window = Tk()
+
+
+def kg_to_grams_pounds_ounces():
+    print('Success!')
+    grams = int(e1_value.get()) * 1000
+    pounds = int(e1_value.get()) * 2.20462
+    ounces = int(e1_value.get()) * 35.274
+    t1.delete('1.0', END) # Deletes the content of the Text box from start to END
+    t1.insert(END, grams)
+    t2.delete('1.0', END)
+    t2.insert(END, pounds)
+    t3.delete('1.0', END)
+    t3.insert(END, ounces)
+
+
+lbl = Label(window, text='Kg')
+lbl.grid(row=0, column=0)
+
+# create button
+btn = Button(window, text='Convert', command=kg_to_grams_pounds_ounces)
+btn.grid(row=0, column=2)
+
+# Entry - Пользовательский ввод
+e1_value = StringVar()
+e1 = Entry(window, textvariable=e1_value)
+e1.grid(row=0, column=1)
+
+# fields that will be filled by results
+t1 = Text(window, height=1, width=20)
+t1.grid(row=1, column=0)
+t2 = Text(window, height=1, width=20)
+t2.grid(row=1, column=1)
+t3 = Text(window, height=1, width=20)
+t3.grid(row=1, column=2)
+
+
+# This makes sure to keep the main window open
+window.mainloop()
